e5bb221131f594095c3260dc8d43250160bab4ff
jcgonzalezsanchez <54146783+jcgonzalezsanchez@users.noreply.github.com>
2020-05-24 19:46:29 -0500

    24-05-2020

--- AnimalCountryWebApp/ACWebApp/wwwroot/js/site.js
+++ AnimalCountryWebApp/ACWebApp/wwwroot/js/site.js
@@ -9,3 +9,31 @@
 //        yearRange: [2000, 2020],
 //        format: 'dd/mm/yyyy'
 //    });
+
+$(document).ready(function () {
+    $('#datepicker').datepicker({
+        uiLibrary: 'bootstrap4'
+    });
+});
+
+
+
+//Search
+
+$("#txtsearch").on("keyup", function () {
+    var validChars = "0123456789";
+    var txtenter = $(this).val().toString();
+    $("table tr").each(function (results) {
+        if (results !== 0) {
+            var id = $(this).find("td:nth-child(2)").text();
+            var id2 = $(this).find("td:nth-child(1)").text();
+            if (id.indexOf(txtenter) !== 0 && id.toLowerCase().indexOf(txtenter.toLowerCase()) < 0 &&
+                id2.indexOf(txtenter) !== 0 && id2.toLowerCase().indexOf(txtenter.toLowerCase()) < 0) {
+                $(this).hide();
+            }
+            else {
+                $(this).show();
+            }
+        }
+    });
+});
